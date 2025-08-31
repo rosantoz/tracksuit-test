ba81f81c19c9d1b8b5d9345fda8cea891b1a55ca
Rodrigo dos Santos <rodrigo@simplywallst.com>
2025-08-31 17:29:49 +1000

    chore: remove logging

--- server/operations/list-insights.ts
+++ server/operations/list-insights.ts
@@ -1,12 +1,10 @@
 import type { Insight } from "$models/insight.ts";
-import type { HasDBClient } from "../shared.ts";
 import type * as insightsTable from "$tables/insights.ts";
+import type { HasDBClient } from "../shared.ts";
 
 type Input = HasDBClient;
 
 export default (input: Input): Insight[] => {
-  console.log("Listing insights");
-
   const rows = input.db.sql<insightsTable.Row>`SELECT * FROM insights`;
 
   const result: Insight[] = rows.map((row) => ({
@@ -14,6 +12,5 @@ export default (input: Input): Insight[] => {
     createdAt: new Date(row.createdAt),
   }));
 
-  console.log("Retrieved insights successfully: ", result);
   return result;
 };
